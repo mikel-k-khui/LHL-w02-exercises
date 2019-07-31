f048f148a5472c4bb921d62a2c0acca2b72a1f99
Mike Chui <mikel.k.khui@gmail.com>
2019-07-31 14:23:25 -0600

    learn to use GET /path-to-page HTTP/1.1\r\n and Request

--- d03-network/requestExample/requestExample.js
+++ d03-network/requestExample/requestExample.js
@@ -5,6 +5,8 @@
  * HTTP servers typically run on port 80
  */
 const net = require('net');
+const request = require('request');
+
 const conn = net.createConnection({ 
   host: 'example.com',
   port: 80
@@ -17,6 +19,15 @@ conn.on('connect', () => {
   conn.write(`GET / HTTP/1.1\r\n`);
   conn.write(`Host: example.com\r\n`);
   conn.write(`\r\n`);
+
+  let URL = 'https://www.google.com/fdsafsafsa.html. ';
+  request(URL, (error, response, body) => {
+    if (!error) {
+      // console.log('body:', body); // Print the HTML for the Google homepage.
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    }
+    console.log('error:', error); // Print the error if one occurred
+  });
 });
 
 /** 
@@ -25,7 +36,6 @@ conn.on('connect', () => {
  * Print the data to the screen, and end the connection
  */
 conn.on('data', (data) => {
-  console.log(data);
+  // console.log(data);
   conn.end();
 });
-

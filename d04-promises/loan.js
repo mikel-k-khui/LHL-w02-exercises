5e266f4d47c6622169b37e7ba2e8484a0442bfb7
Mike Chui <mikel.k.khui@gmail.com>
2019-08-01 19:57:19 -0600

    Practiced promises with resolve and reject

---
+++ d04-promises/loan.js
@@ -0,0 +1,28 @@
+let creditLimit = 50;
+
+/*
+ * Input: number of dollars to loan out
+ * Returns: Promise of loan which may or may not fulfill, based on remaining credit.
+ * If creditLimit is less than the requested amount, only the remaining limit is loaned out, otherwise the full amount is loaned out. If $0 remain in the limit, the loan request is rejected (error!).
+ */
+const loanOut = function(amount) {
+  return new Promise((resolve, reject) => {
+
+    if (creditLimit <= 0) reject("Insufficient Funds!");
+    else {
+      if (creditLimit > 0 && amount > creditLimit) amount = creditLimit;
+      //reduced the amount requested to the credit limit and carry on the resolve
+      creditLimit -= amount;
+      resolve(amount);
+    }
+  });
+};
+
+console.log("Asking for $150, which should be okay ...");
+loanOut(150)
+  .then((amountReceived) => {
+    console.log(`\t-> I got $${amountReceived} loan from the bank! Remaining Credit Limit: $${creditLimit}`);
+  })
+  .catch((err) => {
+    console.log(`\t-> Error: ${err}!`);
+  });
